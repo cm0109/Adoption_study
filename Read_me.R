18e9ad3aa0967416d59cb16d52e4cb02b342b8b5
Chiranjit Mukherjee <chiranjit.m@gmail.com>
2019-11-19 23:43:07 -0500

    Initial commit

---
+++ Read_me.R
@@ -0,0 +1,32 @@
+
+# All input files for main Markdown script 
+
+# ISR ASVs for 778 samples
+ISR_seqtab_atab_comb_filtp_clean.txt
+
+# Clinical Metadata for 265 subjects
+AD_clinical_meta_2019_ISR.txt
+
+# Mother-Child Metadata (Age Matched)
+AD_clinical_meta_2019_ISR_mch.txt
+
+# Metadata for bio M-F-Ch families (3fam)
+ISR_sal_meta_bio_3fam.txt
+
+# Metadata for siblings
+ISR_sal_meta_bio_sibs.txt
+
+# Metadata for couples
+ISR_sal_meta_bio_cpls.txt
+
+# Technical replicates data
+tr_prab_dist_melt_ss_sal.txt
+
+# ISR Reference Sequence Database Taxonomy File
+ISR_db_oct2018_tax.txt
+
+# ISR Blast Results
+ISR_sal_blast_top2.txt
+
+# 16S V1-V3 Core Matched Counts (709 samples)
+AD_core_counts.txt
